dc11f2df5d56b1c2253bc9fe616d910252a9bab8
Bemnet57 <bemnetasseged57@gmail.com>
2025-08-06 22:27:16 +0300

    created initial models for users app

--- book_club_project/users/models.py
+++ book_club_project/users/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, phone, password=None, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email")
+        user = self.model(
+            email=self.normalize_email(email),
+            full_name=full_name,
+            phone=phone,
+            is_admin=is_admin
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=10, unique=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name', 'phone']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.full_name
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
