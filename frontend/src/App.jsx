3cf354e2c86a0bc78ce1441b6d3ec39f4f2d2e72
BeamlakF <beamlakfekadu562@gmail.com>
2025-08-22 15:19:30 +0300

    edit navbar and add Book order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,9 @@ import Navbar from "./components/Navbar_temp";
 import Footer from "./components/Footer";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import './App.css';
+import BookOrderPage from "./pages/BookOrderPage";
 
 
 function App() {
@@ -12,12 +13,13 @@ function App() {
     <BrowserRouter>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <Home/>
+        {/* <Home/> */}
         <SignUp/>
         <Login/>
         <main className="flex-grow flex items-center justify-center">
           <h1 className="text-3xl font-bold">Hello React + Django </h1>
         </main>
+        <BookOrderPage />
         <Footer />
       </div>
     </BrowserRouter>
--- frontend/src/components/Navbar_temp.jsx
+++ frontend/src/components/Navbar_temp.jsx
@@ -1,19 +1,54 @@
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container">
+        {/* Logo */}
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src="/logo.png"
+            alt="5k Gibi Gubae"
+            width="40"
+            height="40"
+            className="d-inline-block align-top me-2"
+          />
+          <span>5k Gibi Gubae Book Store</span>
+        </Link>
 
-function Navbar(){
-    return (
-        <nav className = "navabar">
-            <img src = "/logo.png" alt = "5k gibi gubae" />
-            <h1 className ="project-title"> 5k Gibi Gubae Book Store</h1>
-            <div className = "links">
-                < Link to = "/">Home</Link>
-                < Link to = "/">Profile</Link>
-                < Link to = "/">Cart</Link>
-                < Link to = "/"> Delivery History</Link>
-            </div>
-        </nav>
-    )
+        {/* Toggler for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Links */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/profile">Profile</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">Cart</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/delivery-history">Delivery History</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
